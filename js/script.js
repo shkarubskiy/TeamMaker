486a55140f74ab0ddd03998b7bb415494dd6158f
shkarubskiy <mail@vshka.ru>
2022-06-08 15:13:13 +0600

    feat: migration to vue

--- js/script.js
+++ js/script.js
@@ -1,147 +1,122 @@
-let _gameData = {
-  players: [],
-  firstTeam: [],
-  secondTeam: [],
-  recentPlayers: loadLocalArray("recentPlayers"),
-};
+const app = Vue.createApp({
+  data() {
+    return {
+      // game data
+      game: {
+        teams: [[], []],
+        players: [],
+      },
+      // screens for visibility control
+      screens: {
+        players: true,
+        teams: false,
+      },
+      // input to get value
+      input: "",
+    };
+  },
+  computed: {
+    // recent players for datalist
+    recentPlayers() {
+      return this.loadLocalArray("recentPlayers").sort();
+    },
 
-loadRecentDataList("playersNames");
-hideElement("playersList");
-hideElement("playersButtonAssign");
-hideElement("teams");
+    // elements for visibility control
+    playersList() {
+      return this.game.players.length > 0 ? true : false;
+    },
 
-function loadLocalArray(key) {
-  if (localStorage.getItem(key) != null)
-    return localStorage.getItem(key).split(",");
-  else return [];
-}
+    playersButtonAssign() {
+      return this.game.players.length > 3 ? true : false;
+    },
+  },
+  methods: {
+    loadLocalArray(key) {
+      if (localStorage.getItem(key) != null)
+        return localStorage.getItem(key).split(",");
+      else return [];
+    },
 
-function saveLocalArray(key, array) {
-  localStorage.setItem(key, array.toString());
-}
+    saveLocalArray(key, array) {
+      localStorage.setItem(key, array.toString());
+    },
 
-function addItemToList(string, id) {
-  let item = document.createElement("li");
-  let deleteButton = document.createElement("a");
+    deletePlayerFromList(index) {
+      this.game.players.splice(index, 1);
+    },
 
-  item.innerHTML = string + deleteButton;
-  deleteButton.href = "#";
-  deleteButton.innerText = " X";
-  deleteButton.onclick = function () {
-    this.parentNode.remove();
-    _gameData.players.splice(_gameData.players.indexOf(string), 1);
-    if (!checkForChildren("playersList")) {
-      hideElement("playersList");
-      hideElement("playersButtonAssign");
-    }
-  };
+    validateInput(string) {
+      if (this.game.players.includes(string))
+        return { result: false, desc: "Имя не должно повторяться" };
+      if (string == "")
+        return { result: false, desc: "Имя не может быть пустым" };
+      if (string.includes(","))
+        return { result: false, desc: "Имя не может содержать запятые" };
+      if (string.includes("|"))
+        return { result: false, desc: "Имя не может содержать символ |" };
+      if (string.length > 18)
+        return { result: false, desc: "Имя слишком длинное" };
+      return { result: true, desc: "OK" };
+    },
 
-  if (!_gameData.recentPlayers.includes(string)) {
-    saveRecentDataList(string);
-  }
-  document.getElementById(id).appendChild(item);
-  document.getElementById(id).lastChild.appendChild(deleteButton);
-}
+    getRandomInt(min, max) {
+      let rand = min - 0.5 + Math.random() * (max - min + 1);
+      return Math.round(rand);
+    },
 
-function loadRecentDataList(id) {
-  let array = _gameData.recentPlayers.sort();
-  for (let i = 0; i < array.length; i++) {
-    let item = document.createElement("option");
-    item.value = array[i];
-    document.getElementById(id).appendChild(item);
-  }
-}
+    addPlayerToList() {
+      if (this.validateInput(this.input).result) {
+        this.game.players.push(this.input);
+        this.input = "";
+        this.$refs.input.focus();
+      } else {
+        alert(this.validateInput(this.input).desc);
+      }
+    },
 
-function saveRecentDataList(string) {
-  _gameData.recentPlayers.push(string);
-  saveLocalArray("recentPlayers", _gameData.recentPlayers);
-}
+    validatePlayersList(array) {
+      return array.length < 4
+        ? { result: false, desc: "Нужно больше игроков (Минимум 4)!" }
+        : { result: true, desc: "OK" };
+    },
 
-function checkInput(string) {
-  if (_gameData.players.includes(string))
-    return { result: false, desc: "Имя не должно повторяться" };
-  if (string == "") return { result: false, desc: "Имя не может быть пустым" };
-  if (string.includes(","))
-    return { result: false, desc: "Имя не может содержать запятые" };
-  if (string.includes("|"))
-    return { result: false, desc: "Имя не может содержать символ |" };
-  if (string.length > 18) return { result: false, desc: "Имя слишком длинное" };
-  return { result: true, desc: "Все корректно" };
-}
+    assignTeams(array) {
+      if (this.validatePlayersList(array).result) {
+        let buffer = array.slice(0);
+        while (buffer.length > 0) {
+          let rnd = this.getRandomInt(0, buffer.length - 1);
+          this.game.teams[0].length <= this.game.teams[1].length
+            ? this.game.teams[0].push(buffer[rnd])
+            : this.game.teams[1].push(buffer[rnd]);
+          buffer.splice(rnd, 1);
+        }
+        this.saveLocalArray("firstTeam", this.game.teams[0]);
+        this.saveLocalArray("secondTeam", this.game.teams[1]);
+        this.saveLocalArray("recentPlayers", this.getRecentPlayers());
+        this.screens.players = false;
+        this.screens.teams = true;
+      } else alert(this.validatePlayersList(array).desc);
+    },
 
-function getRandomInt(min, max) {
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
-}
+    clearLocalData() {
+      localStorage.clear();
+    },
 
-function hideElement(id) {
-  document.getElementById(id).classList.add("hidden");
-}
+    restoreLocalData() {
+      this.game.teams[0] = this.loadLocalArray("firstTeam");
+      this.game.teams[1] = this.loadLocalArray("secondTeam");
+      this.screens.players = false;
+      this.screens.teams = true;
+    },
 
-function showElement(id) {
-  document.getElementById(id).classList.remove("hidden");
-}
-
-function checkForChildren(id) {
-  if (document.getElementById(id).childNodes.length > 0) return true;
-  else return false;
-}
-
-function assignTeams(array) {
-  if (array.length > 3) {
-    let buffer = array;
-    while (buffer.length > 0) {
-      let rnd = getRandomInt(0, buffer.length - 1);
-      if (_gameData.firstTeam.length <= _gameData.secondTeam.length)
-        _gameData.firstTeam.push(buffer[rnd]);
-      else _gameData.secondTeam.push(buffer[rnd]);
-      buffer.splice(rnd, 1);
-    }
-    return true;
-  } else alert("Нужно больше игроков (Минимум 4)!");
-  return false;
-}
-
-function fillTeamList(id, array) {
-  for (let element of array) {
-    let item = document.createElement("li");
-    item.innerHTML = element;
-    document.getElementById(id).appendChild(item);
-  }
-}
-
-playersButtonAdd.onclick = function () {
-  let input = document.getElementById("playersInput");
-  if (checkInput(input.value).result) {
-    _gameData.players.push(input.value);
-    addItemToList(input.value, "playersList");
-    input.value = "";
-    input.focus();
-  } else {
-    alert(checkInput(input.value).desc);
-  }
-  if (checkForChildren("playersList")) {
-    showElement("playersList");
-    showElement("playersButtonAssign");
-  }
-};
-
-playersButtonAssign.onclick = function () {
-  if (assignTeams(_gameData.players)) {
-    fillTeamList("teamsListFirst", _gameData.firstTeam);
-    fillTeamList("teamsListSecond", _gameData.secondTeam);
-    hideElement("players");
-    showElement("teams");
-    saveLocalArray("firstTeam", _gameData.firstTeam);
-    saveLocalArray("secondTeam", _gameData.secondTeam);
-  }
-};
-
-spoilerButtonClear.onclick = function () {
-  localStorage.clear();
-};
-
-spoilerButtonRestore.onclick = function () {
-  fillTeamList("listFirstTeam", loadLocalArray("firstTeam"));
-  fillTeamList("listSecondTeam", loadLocalArray("secondTeam"));
-};
+    getRecentPlayers() {
+      let recentPlayers = this.loadLocalArray("recentPlayers");
+      for (let i = 0; i < this.game.players.length; i++) {
+        if (!recentPlayers.includes(this.game.players[i]))
+          recentPlayers.push(this.game.players[i]);
+      }
+      return recentPlayers;
+    },
+  },
+});
+app.mount("#app");
